afeba61be77f235ce89b5d76baa8b6bc270e44ca
Kilamper <kilamper@gmail.com>
2023-11-04 14:21:46 +0000

    Final Person Commit

--- src/software/ulpgc/kata1/Person.java
+++ src/software/ulpgc/kata1/Person.java
@@ -1,4 +1,37 @@
 package software.ulpgc.kata1;
 
+import java.time.LocalDate;
+
 public class Person {
+    private final String name;
+    private final LocalDate birthday;
+
+    public Person(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public int getAge() {
+        return toYears(LocalDate.now().toEpochDay() - getBirthday().toEpochDay());
+    }
+
+    private int toYears(long age) {
+        return (int) (age/365.25);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birthday=" + birthday +
+                ", age=" + getAge() + '}';
+    }
 }
